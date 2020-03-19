dcd3f1fd6709036f48b2bbba60d7ec4bb3e964c4
KLisabeth <professional.translations4ppl@gmail.com>
2020-03-19 23:29:18 +0100

    display-matching city with a string returnig func

--- handler/find-match.js
+++ handler/find-match.js
@@ -5,3 +5,23 @@ function findMatches(wordToMatch, cities) {
     return place.city.match(regex) || place.state.match(regex)
   });
 }
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities); //will display what it matches
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi'); // will find what ever it match and will highlight it
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`); 
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span> 
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
